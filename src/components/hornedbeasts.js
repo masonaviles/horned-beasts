30e784a484f957d4187a480bb2beaabed4e3e160
Mason Aviles <masonaviles@Masons-MacBook-Pro.local>
2021-03-16 16:29:34 -0700

    working on index arrays, add bootstrap

--- src/components/hornedbeasts.js
+++ src/components/hornedbeasts.js
@@ -3,26 +3,33 @@ import Data from '../data/data';
 
 class HornedBeasts extends React.Component {
   state = {
-    numberOfLikes: 0
+    numberOfLikes: 0,
+    selectedBeast: ''
   }
 
   likeBeast = () => {
-    this.setState({ numberOfLikes: this.state.numberOfLikes + 1 });
+    this.setState( prevState => ({
+      numberOfLikes: prevState.numberOfLikes + 1
+    }));
+    console.log('test');
   }
 
   render() {
-    const beasts = Data.map((beast) =>
-      <li>
+    const beasts = Data.map((beast, index) =>
+      <div key={index}>
         <h3>{beast.title}</h3>
-        <div>❤️ {this.state.numberOfLikes}</div>
-        <img src={beast.image_url} alt={beast.description} title={beast.title} onClick={this.likeBeast}/>
+        <div>
+          <button onClick={this.likeBeast}>❤️</button>
+          <span>{this.state.numberOfLikes}</span>
+        </div>
+        <img src={beast.image_url} alt={beast.description} title={beast.title}/>
         <p>{beast.description}</p>
-      </li>
+      </div>
     );
     return (
-      <ul>
+      <div className="beasts-container">
         {beasts}
-      </ul>
+      </div>
     );
   }
 }
