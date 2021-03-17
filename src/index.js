8bd8b69ba333c37d902182774a63d4b31431400b
Mason Aviles <masonaviles@Masons-MacBook-Pro.local>
2021-03-17 00:37:53 -0700

    git isnt working because I realized it neve renamed my app file from App to app, so I changedthe reference back to App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
-import App from './app';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
