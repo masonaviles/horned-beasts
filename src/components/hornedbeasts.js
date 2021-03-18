3fbec110b03fad5f8c59bcc5a235fe8e8583a256
Mason Aviles <masonaviles@Masons-MacBook-Pro.local>
2021-03-17 21:34:32 -0700

    rearrange map function so it's in the parent and not the child so it's passing props to child

--- src/components/hornedbeasts.js
+++ src/components/hornedbeasts.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
-import Data from '../data/data';
 import Heartlike from './heartlike';
 
 class HornedBeasts extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      favorites: 0
+    }
+  }
+
+  addFavorite = () => {
+    this.setState({ favorites: this.state.favorites + 1 });
+  }
+
   render() {
-    const beasts = Data.map((beast) =>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={beast.image_url}  alt={beast.description} title={beast.title}/>
-        <Card.Body>
-          <Card.Title>{beast.title}</Card.Title><Heartlike />
-          <Card.Text>
-            {beast.description}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    );
+    // const beasts = Data.map((beast) =>
+    //   <Card style={{ width: '18rem' }}>
+    //     <Card.Img variant="top" src={beast.image_url}  alt={beast.description} title={beast.title}/>
+    //     <Card.Body>
+    //       <Card.Title>{beast.title}</Card.Title><Heartlike />
+    //       <Card.Text>
+    //         {beast.description}
+    //       </Card.Text>
+    //     </Card.Body>
+    //   </Card>
+    // );
     return (
       <div className="beasts-container">
-        {beasts}
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={this.props.src}  alt={this.props.description} title={this.props.title}/>
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title><Heartlike />
+            <Card.Text>
+              {this.props.description}
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
--- src/views/main.js
+++ src/views/main.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import CardDeck from 'react-bootstrap/CardDeck'
 import HornedBeasts from '../components/hornedbeasts';
+import rawData from '../data/data';
 
 class Main extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      gallery: rawData
+    }
+  }
 
   render() {
     return(
       <main>
-        <div className='wrapper'>
-          <HornedBeasts/>
-        </div>
+        <div id="wrapper">
+        <CardDeck>
+        {rawData.map((beast, index) => (
+          <div key={index}>
+            <HornedBeasts 
+              src = {beast.image_url}
+              title = {beast.title}
+              description = {beast.description}
+            />
+          </div>
+        ))
+
+        }
+        </CardDeck>
+      </div>
       </main>
     );
   }
